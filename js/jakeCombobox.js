b590fe0d1aa6392f4a830bed9fdf17cc0efb28a1
Jake Crane <jake@jakecrane.com>
2016-11-06 21:31:34 -0600

    Refactoring and handle non matches better

--- js/jakeCombobox.js
+++ js/jakeCombobox.js
@@ -5,9 +5,9 @@
 
     $.widget("jake.combobox", {
 
+        downArrow: '&#x25BC;',
+
         options: {
-            //down arrow
-            buttonText: '&#x25BC;',
             isOpen: false,
             change: null
         },
@@ -17,10 +17,10 @@
             $select.hide();
 
             var combobox = this._createComboBox();
-            this.combobox = $(combobox);
+            this.$combobox = $(combobox);
             this._cacheElements();
             this._bindUIActions();
-            $select.after(this.combobox);
+            $select.after(this.$combobox);
             this._refresh();
         },
 
@@ -28,15 +28,15 @@
         },
 
         _destroy: function () {
-            this.combobox.remove();
+            this.$combobox.remove();
             this.element.show();
         },
 
         _createLi: function (val, text) {
             return ''
-                + '\t<li data-value="' + val + '">'
+                + '<li data-value="' + val + '">'
                 + '<span data-value="' + val + '">' + text + '</span>'
-                + '</li>\n';
+                + '</li>';
         },
 
         _createLis: function (options) {
@@ -51,25 +51,25 @@
 
         _createComboBox: function () {
             var combobox = ''
-                + '<div class="combobox">\n'
-                + '\t<input type="text" class="txtbox" />\n'
-                + '\t<a href="#" class="txtbox-btn">' + this.options.buttonText + '</a>\n'
-                + '</div>\n'
-                + '<ul class="combobox-options">\n'
+                + '<div class="combobox">'
+                + '<input type="text" class="txtbox" />'
+                + '<a href="#" class="txtbox-btn">' + this.downArrow + '</a>'
+                + '</div>'
+                + '<ul class="combobox-options">'
                 + this._createLis(this.element.children('option'))
-                + '</ul>\n';
+                + '</ul>';
             return combobox;
         },
 
         _openClose: function () {
-            this.cached['.combobox-options'].slideToggle(300);
+            this.elements.$options.slideToggle(300);
         },
 
         _selectLi: function (e) {
             var $selectedLi = $(e.target),
                 selectedValue = $selectedLi.data('value');
 
-            this.cached['.txtbox'].val(selectedValue);
+            this.elements.$txtbox.val(selectedValue);
             this._openClose();
 
             this.element.val(selectedValue);
@@ -78,43 +78,43 @@
         },
 
         _autocomplete: function () {
-            var term = this.cached['.txtbox'].val(),
-                $results = null;
+            var term = this.elements.$txtbox.val();
+            var $results = null;
 
             if (term !== '') {
-                $results = this.cached['.combobox-options li'].find('span:icontains(' + term + ')');
+                $results = this.elements.$lis.find('span:icontains(' + term + ')');
             }
-            if ($results) {
-                this.cached['.combobox-options'].show();
-                this.cached['.combobox-options li'].show();
-                this.cached['.combobox-options li'].children().show();
+            if ($results && $results.length > 0) {
+                this.elements.$options.show();
+                this.elements.$lis.show();
+                this.elements.$lis.children().show();
 
-                var $spans = this.cached['.combobox-options li'].children().not($results);
+                var $spans = this.elements.$lis.children().not($results);
                 $spans.parent().hide();
                 $spans.hide();
             } else {
-                this.cached['.combobox-options'].hide();
-                this.cached['.combobox-options li'].show();
-                this.cached['.combobox-options li'].children().show();
+                this.elements.$options.hide();
+                this.elements.$lis.show();
+                this.elements.$lis.children().show();
             }
         },
 
         _bindUIActions: function () {
 
-            this.cached['.combobox-options'].hide();
+            this.elements.$options.hide();
             if (this.options.isOpen) {
-                this.cached['.combobox-options'].show();
+                this.elements.$options.show();
             }
 
-            this._on(this.cached['.txtbox-btn'], {
+            this._on(this.elements.$txtboxBtn, {
                 click: '_openClose'
             });
 
-            this._on(this.cached['.combobox-options li'], {
+            this._on(this.elements.$lis, {
                 click: '_selectLi'
             });
 
-            this._on(this.cached['.txtbox'], {
+            this._on(this.elements.$txtbox, {
                 keyup: '_autocomplete'
             })
         },
@@ -137,17 +137,13 @@
         },
 
         _cacheElements: function () {
-            var $combobox = this.combobox;
-            var $txtboxBtn = $combobox.find('.txtbox-btn');
-            var $txtbox = $combobox.find('.txtbox');
-            var $options = $combobox.siblings('.combobox-options');
-            var $lis = $options.children();
-
-            this.cached = {
-                ".txtbox-btn": $txtboxBtn,
-                ".combobox-options": $options,
-                ".combobox-options li": $lis,
-                ".txtbox": $txtbox
+            var $options = this.$combobox.siblings('.combobox-options');
+            this.elements = {
+                $txtboxBtn: this.$combobox.find('.txtbox-btn'),
+                $txtbox: this.$combobox.find('.txtbox'),
+                $options: $options,
+                $lis: $options.children(),
+                ".txtbox": this.$combobox.find('.txtbox')
             };
 
         }
